tamentis/rpdb

commit a5d81372cf4bbe7d13fdeea56de1eebd3299647b

fix: multiple instances of continue is working

rpdb/__init__.py
@@ -3,19 +3,93 @@
 __author__ = "Bertrand Janin <b@janin.com>"
 __version__ = "0.1.6"
 
+from contextlib import contextmanager
 import pdb
 import socket
 import threading
 import signal
 import sys
 import traceback
+import os
+import typing as t
 from functools import partial
 
 DEFAULT_ADDR = "127.0.0.1"
 DEFAULT_PORT = 4444
 
 
+def get_default_address():
+    return os.environ.get("PYTHON_RPDB_ADDRESS", DEFAULT_ADDR)
+
+
+def get_default_port():
+    return int(os.environ.get("PYTHON_RPDB_PORT", DEFAULT_PORT))
+
+
+def safe_print(msg):
+    # Writes to stdout are forbidden in mod_wsgi environments
+    try:
+        sys.stderr.write("[rpdb] " + msg + "\n")
+    except IOError:
+        pass
+
+
+# https://github.com/gotcha/ipdb/blob/400e37c56c9772fdc4c04ddb29d8a4a20568fb1a/ipdb/__main__.py#L233-L246
+@contextmanager
+def launch_ipdb_on_exception():
+    try:
+        yield
+    except Exception:
+        post_mortem()
+    finally:
+        pass
+
+
+# iex is a concise alias
+iex = launch_ipdb_on_exception()
+
+
+def ipython_available():
+    try:
+        from IPython.core.debugger import Pdb
+
+        return True
+    except ImportError:
+        return False
+
+
+def get_debugger_class():
+    # TODO shell = get_ipython() to detect if we are in ipython
+    # from IPython.terminal.debugger import TerminalPdb
+
+    debugger_base = pdb.Pdb
+
+    if ipython_available():
+        from IPython.core.debugger import Pdb
+
+        debugger_base = Pdb
+
+    class Debugger(Rpdb, debugger_base):
+        def __init__(self, addr=None, port=None):
+            Rpdb.__init__(self, addr=addr, port=port, debugger_base=debugger_base)
+
+    return Debugger
+
+
+def shutdown_socket(sock: socket.socket):
+    try:
+        sock.shutdown(socket.SHUT_RDWR)
+    except OSError:
+        pass  # Socket is already closed
+    finally:
+        sock.close()
+
+
 class FileObjectWrapper(object):
+    """
+    A wrapper class for file objects that provides access to both the wrapped file object and standard I/O streams.
+    """
+
     def __init__(self, fileobject, stdio):
         self._obj = fileobject
         self._io = stdio
@@ -30,14 +104,20 @@ class FileObjectWrapper(object):
         return attr
 
 
-class Rpdb(pdb.Pdb):
-
-    def __init__(self, addr=DEFAULT_ADDR, port=DEFAULT_PORT):
+class Rpdb:
+    def __init__(self, addr=None, port=None, debugger_base=t.Type[pdb.Pdb]):
         """Initialize the socket and initialize pdb."""
 
+        self.debugger = debugger_base
+
+        addr = addr or get_default_address()
+        port = port or get_default_port()
+
+        safe_print(f"attempting to bind {addr}:{port}")
+
         # Backup stdin and stdout before replacing them by the socket handle
-        self.old_stdout = sys.stdout
-        self.old_stdin = sys.stdin
+        self.dup_stdout_fileno = os.dup(sys.stdout.fileno())
+        self.dup_stdin_fileno = os.dup(sys.stdin.fileno())
         self.port = port
 
         # Open a 'reusable' socket to let the webapp reload on the same port
@@ -46,35 +126,51 @@ class Rpdb(pdb.Pdb):
         self.skt.bind((addr, port))
         self.skt.listen(1)
 
-        # Writes to stdout are forbidden in mod_wsgi environments
-        try:
-            sys.stderr.write("pdb is running on %s:%d\n"
-                             % self.skt.getsockname())
-        except IOError:
-            pass
+        safe_print("running on %s:%d\n" % self.skt.getsockname())
 
         (clientsocket, address) = self.skt.accept()
-        handle = clientsocket.makefile('rw')
-        pdb.Pdb.__init__(self, completekey='tab',
-                         stdin=FileObjectWrapper(handle, self.old_stdin),
-                         stdout=FileObjectWrapper(handle, self.old_stdin))
-        sys.stdout = sys.stdin = handle
-        self.handle = handle
+        self.clientsocket = clientsocket
+        handle = clientsocket.makefile("rw")
+
+        self.debugger.__init__(
+            self,
+            completekey="tab",
+            stdin=handle,
+            stdout=handle,
+        )
+
+        # overwrite the default stdout and stdin with the socket file handles
+        # if this isn't done, any other interactive programs (like `interact` or ipy) will
+        os.dup2(clientsocket.fileno(), sys.stdout.fileno())
+        os.dup2(clientsocket.fileno(), sys.stdin.fileno())
+
         OCCUPIED.claim(port, sys.stdout)
 
     def shutdown(self):
         """Revert stdin and stdout, close the socket."""
-        sys.stdout = self.old_stdout
-        sys.stdin = self.old_stdin
-        self.handle.close()
+        safe_print("shutting down\n")
+
+        # restore original stdout and stdin since we are exiting debug mode
+        os.dup2(self.dup_stdout_fileno, sys.stdout.fileno())
+        os.dup2(self.dup_stdin_fileno, sys.stdin.fileno())
+
+        # `shutdown` on the `skt` will trigger an error
+        # if you don't `shutdown` the `clientsocket` socat & friends will hang
+        # self.clientsocket.shutdown(socket.SHUT_RDWR)
+        # self.clientsocket.close()
+        shutdown_socket(self.clientsocket)
+
+        # self.skt.shutdown(socket.SHUT_RDWR)
+        # self.skt.close()
+        shutdown_socket(self.skt)
+
         OCCUPIED.unclaim(self.port)
-        self.skt.shutdown(socket.SHUT_RDWR)
-        self.skt.close()
 
     def do_continue(self, arg):
         """Clean-up and do underlying continue."""
+
         try:
-            return pdb.Pdb.do_continue(self, arg)
+            return self.debugger.do_continue(self, arg)
         finally:
             self.shutdown()
 
@@ -83,7 +179,7 @@ class Rpdb(pdb.Pdb):
     def do_quit(self, arg):
         """Clean-up and do underlying quit."""
         try:
-            return pdb.Pdb.do_quit(self, arg)
+            return self.debugger.do_quit(self, arg)
         finally:
             self.shutdown()
 
@@ -92,27 +188,42 @@ class Rpdb(pdb.Pdb):
     def do_EOF(self, arg):
         """Clean-up and do underlying EOF."""
         try:
-            return pdb.Pdb.do_EOF(self, arg)
+            return self.debugger.do_EOF(self, arg)
         finally:
             self.shutdown()
 
+    # TODO best approach here would be to bind the interactive ipy session to the new pipes we setup
+    # def do_interact(self, arg):
+    #     ipshell = embed.InteractiveShellEmbed(
+    #         config=self.shell.config,
+    #         banner1="*interactive*",
+    #         exit_msg="*exiting interactive console...*",
+    #     )
+    #     global_ns = self.curframe.f_globals
+    #     ipshell(
+    #         module=sys.modules.get(global_ns["__name__"], None),
+    #         local_ns=self.curframe_locals,
+    #     )
 
-def set_trace(addr=DEFAULT_ADDR, port=DEFAULT_PORT, frame=None):
+
+def set_trace(addr=None, port=None, frame=None):
     """Wrapper function to keep the same import x; x.set_trace() interface.
 
     We catch all the possible exceptions from pdb and cleanup.
 
     """
     try:
-        debugger = Rpdb(addr=addr, port=port)
-    except socket.error:
+        debugger = get_debugger_class()(addr=addr, port=port)
+    except socket.error as e:
         if OCCUPIED.is_claimed(port, sys.stdout):
             # rpdb is already on this port - good enough, let it go on:
-            sys.stdout.write("(Recurrent rpdb invocation ignored)\n")
+            safe_print("(Recurrent rpdb invocation ignored)\n")
             return
         else:
             # Port occupied by something else.
-            raise
+            safe_print("Target port is already in use. Original error: %s\n" % e)
+            return
+
     try:
         debugger.set_trace(frame or sys._getframe().f_back)
     except Exception:
@@ -123,15 +234,16 @@ def _trap_handler(addr, port, signum, frame):
     set_trace(addr, port, frame=frame)
 
 
-def handle_trap(addr=DEFAULT_ADDR, port=DEFAULT_PORT):
+def handle_trap(addr=None, port=None):
     """Register rpdb as the SIGTRAP signal handler"""
     signal.signal(signal.SIGTRAP, partial(_trap_handler, addr, port))
 
 
-def post_mortem(addr=DEFAULT_ADDR, port=DEFAULT_PORT):
-    debugger = Rpdb(addr=addr, port=port)
+def post_mortem(addr=None, port=None):
     type, value, tb = sys.exc_info()
     traceback.print_exc()
+
+    debugger = get_debugger_class()(addr=addr, port=port)
     debugger.reset()
     debugger.interaction(None, tb)
 
@@ -157,14 +269,15 @@ class OccupiedPorts(object):
 
     def is_claimed(self, port, handle):
         self.lock.acquire(True)
-        got = (self.claims.get(port) == id(handle))
+        got = self.claims.get(port) == id(handle)
         self.lock.release()
         return got
 
     def unclaim(self, port):
         self.lock.acquire(True)
-        del self.claims[port]
+        self.claims.pop(port, None)
         self.lock.release()
+
 
 # {port: sys.stdout} pairs to track recursive rpdb invocation on same port.
 # This scheme doesn't interfere with recursive invocations on separate ports -

commit d1721866a84992316ff537064297411e04046bd8

Fix Rpdb init when debugger_base is wrong.
Broken by https://github.com/tamentis/rpdb/commit/a5d81372cf4bbe7d13fdeea56de1eebd3299647b

rpdb/__init__.py
@@ -105,7 +105,7 @@ class FileObjectWrapper(object):
 
 
 class Rpdb:
-    def __init__(self, addr=None, port=None, debugger_base=t.Type[pdb.Pdb]):
+    def __init__(self, addr=None, port=None, debugger_base: t.Type[pdb.Pdb] = pdb.Pdb):
         """Initialize the socket and initialize pdb."""
 
         self.debugger = debugger_base